76c902010c4b66955e5f190415d324d833c0de18
Ryo <kanadajinlee@yahoo.ca>
2017-05-22 23:15:34 -0500

    Awesomely made awesome progress! Go console-logs

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var $ = cheerio.load('<h2 class="title">Original text</h2>');
+request('https://www.beagreatteacher.com/daily-fun-fact/', function(err, resp, html) {
+    if (!err){
+        $ = cheerio.load(html);
+        // console.log(html);
+        console.log($('main.content').find('h2').text())
+        // console.log($('main.content.0.children'));
+        // console.log($('.logo-subtext').text());
+    }
+});
 
-$('h2.title').text('New stuff!');
-$('h2').addClass('welcome');
+// var $ = cheerio.load('<h2 class="title">Original text</h2>');
 
-console.log($.html()); //<h2 class="title welcome">Hello there!</h2>
+// $('h2.title').text('New stuff!');
+// $('h2').addClass('welcome');
+
+// console.log($.html()); //<h2 class="title welcome">Hello there!</h2>
