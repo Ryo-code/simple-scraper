1556b3fb9e13c59f65e69c3d2c839250e6a9fa65
Ryo <kanadajinlee@yahoo.ca>
2017-05-23 01:21:13 -0500

    Made the whole scraper in less than 10 lines

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ var cheerio = require('cheerio');
 request('https://www.beagreatteacher.com/daily-fun-fact/', function(err, resp, html) {
     if (!err){
         $ = cheerio.load(html);
-        
-        var funFactsObj = $('main.content').find('h2').next().text()
-        // console.log(funFactsObj, "... this is a...", typeof(funFactsObj))
-
         var factOfTheDay = $('span:contains("Random Fact of the Day:")').parent().parent().next().text() //It's weird because of the way their HTML is structured
-        
         console.log("Random fact:", factOfTheDay);
     }
 });
