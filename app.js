f87b6542f5679af998e3e3394ec34f68ec788740
Ryo <kanadajinlee@yahoo.ca>
2017-05-22 22:44:22 -0500

    First commit with a working tiny example of cheerio

---
+++ app.js
@@ -0,0 +1,19 @@
+var cheerio = require('cheerio');
+var request = require('request');
+
+request({
+    method: 'GET',
+    url: 'https://github.com/showcases'
+}, function(err, response, body) {
+    if (err) return console.error(err);
+
+    // Tell Cherrio to load the HTML
+    console.log(body)
+    $ = cheerio.load(body);
+    $('li.collection-card').each(function() {
+            var href = $('a.collection-card-image', this).attr('href');
+            if (href.lastIndexOf('/') > 0) {
+                console.log($('h3', this).text());
+            }
+    });
+});
