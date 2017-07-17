5cf9ed01054cdbc2962790382309b6b36333d145
Ryo <kanadajinlee@yahoo.ca>
2017-07-16 22:05:24 -0500

    Editted the first scraper after testing w\ RN UI

--- app.js
+++ app.js
@@ -9,28 +9,20 @@ var cheerio = require('cheerio');
 var quoteOfTheDay = () => {
   request('https://en.wikiquote.org/wiki/Main_Page', (err, resp, html) => {
     if(!err) {
-      var $ = cheerio.load(html);
-      var QOTDobj = {};
+      const $ = cheerio.load(html);
+      const QOTDobj = {};
+      const todaysQuote = $('small').parent().parent().parent().children().children().children().children().children().children().children().has('td:contains("~")'); //.html() <- To see the HTML structure
 
-      //The code below looks insane because of the way they structured their HTML
-      var quoterElement = $('small').parent().parent().parent().children().children().children().children().children().children().children().has('td:contains("~")');
-      var quotedBy = quoterElement.text().trim().slice(0, -1);
-      var quoteElement = $('small').parent().parent().parent().children().children().children().children().children().children().children().children();
-      var actualQuote = quoteElement.text().trim().slice(0, -quotedBy.length -1);
+      console.log("Quote----------------->:", todaysQuote.children().children().first('td').text().trim());
+      console.log("Quoter----------------->:", todaysQuote.children().children().first('td').next().children().children('a').html());
+      console.log("QuoterLink----------------->:", "https://en.wikiquote.org" + todaysQuote.children().children().first('td').next().children().children('a').attr('href'));
+      console.log("Image source----------------->:", "https:" + todaysQuote.parent().parent().children().children('a').children('img').attr('src') );
 
-      console.log("==================================================")
-      console.log("Quote of the Day...");
-      console.log("");
-      console.log(actualQuote);
-      console.log("");
-      console.log("Quoted by:", quotedBy);
-      console.log("==================================================")
+      const quote = todaysQuote.children().children().first('td').text().trim();
+      const quoter = todaysQuote.children().children().first('td').next().children().children('a').html();
+      const quoterLink = "https://en.wikiquote.org" + todaysQuote.children().children().first('td').next().children().children('a').attr('href');
+      const imageSrc = "https:" + todaysQuote.parent().parent().children().children('a').children('img').attr('src');
 
-      QOTDobj.quote = actualQuote;
-      QOTDobj.quoter = quotedBy;
-
-      console.log("Full QOTDobj object...", QOTDobj)
-      return QOTDobj;
     }
   })
 }
@@ -46,34 +38,24 @@ var merriamWebsterWOTD = () => {
       var wordType = $('.main-attr').text().trim();
       var pronunciation = $('.word-syllables').text().trim();
 
-      //NOTE: Don't change the ORDER of these variables!
       var entireDefinitionsBox = $('.wod-definition-container').text();
       var exampleOne = $('.wod-definition-container h2:contains("Examples")').next().text();
       var exampleTwo = $('.wod-definition-container h2:contains("Examples")').next().next().text();
       var exampleChars = exampleOne.length + exampleTwo.length
       var definitionsOnly = entireDefinitionsBox.slice(35, -exampleChars -50).trim();
-      var didYouKnow = $('.wod-did-you-know-container').children().next().text();
+      var didYouKnow = $('.wod-did-you-know-container').children().next().html();
 
-      // console.log(" ---===< Word of the day:", todaysWord, "(" + wordType + ")" ,"[" + pronunciation + "] >===---");
-      // console.log("~ ~ ~ ~ ~ ~ ~ ~ ~")
-      // console.log("Definitions ====>", definitionsOnly) //You have to experiment with .slice() on different days (you could do -40, -50, etc.)
-      // console.log("~ ~ ~ ~ ~ ~ ~ ~ ~")
-      // console.log("Example 1 -->", exampleOne); //DONE!
-      // console.log("")
-      // console.log("Example 2 -->", exampleTwo); //DONE!
-      // console.log("- - - - - - - - - - - - - - - - - - - - ")
-      // console.log("Food for thought:", didYouKnow); //DONE!!! (though there are no italics)
+      console.log("#####################wotd#####################")
+      console.log(" ---===< Word of the day:", todaysWord, "(" + wordType + ")" ,"[" + pronunciation + "] >===---");
+      console.log("Definitions ====>", definitionsOnly) //You have to experiment with .slice() on different days (you could do -40, -50, etc.)
+      console.log("~ ~ ~ ~ ~ ~ ~ ~ ~")
+      console.log("Example 1 -->", exampleOne);
+      console.log("")
+      console.log("Example 2 -->", exampleTwo);
+      console.log("~ ~ ~ ~ ~ ~ ~ ~ ~")
+      console.log("Food for thought:", didYouKnow);
+      console.log("#####################wotd#####################")
 
-      WOTDobj.wotd = todaysWord;
-      WOTDobj.wordType = wordType;
-      WOTDobj.pronunciation = pronunciation;
-      WOTDobj.definition = definitionsOnly;
-      WOTDobj.example1 = exampleOne;
-      WOTDobj.example2 = exampleTwo;
-      WOTDobj.didYouKnow = didYouKnow;
-
-      console.log("Full WOTDobj object...", WOTDobj)
-      return WOTDobj;
     }
   });
 }
@@ -84,9 +66,10 @@ var beAGreatTeacherFOTD = () => {
     if (!err) {
       var $ = cheerio.load(html);
       var FACTobj = {};
+      var todaysFact = $('span:contains("Random Fact of the Day:")').parent();
 
-      var factOfTheDay = $('span:contains("Random Fact of the Day:")').parent().parent().next().text();
-      var factOfTheDayBackup = $('span:contains("Random Fact of the Day:")').parent().next().text();
+      var factOfTheDay = todaysFact.parent().next().text();
+      var factOfTheDayBackup = todaysFact.next().text();
 
       // console.log("`````````````````````````````````")
       // console.log("Random fact:", factOfTheDay, factOfTheDayBackup); //One of these will work, the other won't (which is perfect, since it seems to randomly alternate)
@@ -117,18 +100,10 @@ var redditTopNews = () => {
       console.log("News Title  -->", newsTitle);
       console.log("News source -->", abbrevLink);
       console.log("Article link ->", fullLink);
-      console.log("`````````````````````````````````");
-      console.log("#of comments ->", commentsNumbers, "...which is a", typeof(commentsNumbers));
+      console.log("#of comments ->", commentsNumbers, "(a", typeof(commentsNumbers), "which is now being parsedinto an integer)");
       console.log("Comments link->", commentsLink);
+      console.log("- - - - - - - - - - - - - - - - - -");
 
-      NEWSobj.title = newsTitle;
-      NEWSobj.source = abbrevLink;
-      NEWSobj.articleLink = fullLink;
-      NEWSobj.numOfRedditComments = parseInt(commentsNumbers);
-      NEWSobj.redditLink = commentsLink;
-
-      console.log("Full NEWSobj object...", NEWSobj);
-      return NEWSobj;
     }
   });
 }
@@ -148,7 +123,7 @@ var dailyCurio = () => {
 
 
 // merriamWebsterWOTD();
-// quoteOfTheDay();
+quoteOfTheDay();
 // beAGreatTeacherFOTD();
-redditTopNews();
+// redditTopNews();
 // dailyCurio();
